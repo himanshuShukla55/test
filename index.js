1cfeaba465c13efab5a41ba0d0e7a790685bc95e
himanshuShukla55 <himanshu.shukla55@gmail.com>
2023-12-15 17:28:02 +0530

    sending an object as response

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.get("/api", (req, res) => {
-  res.status(200).send("Welcome");
+  res.status(200).json({ success: true, data: "Data from server" });
 });
 
 app.listen(PORT, () => {
