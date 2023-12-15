313082e71b9d13882076b64a84b03af7633cd44e
himanshuShukla55 <himanshu.shukla55@gmail.com>
2023-12-15 17:05:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome");
+});
+
+app.listen(PORT, () => {
+  console.log("server is listening on port: ", PORT);
+});
